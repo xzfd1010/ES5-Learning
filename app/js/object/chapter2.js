8067b55b8aadb7914da83e4246af57fc0f4c5a4a
Nick <zhongcheng509@gmail.com>
2017-10-06 14:27:26 +0800

    对象的创建

--- app/js/object/chapter2.js
+++ app/js/object/chapter2.js
@@ -2,7 +2,7 @@
 /* eslint-disable no-redeclare, no-console ,no-unused-vars*/
 var log = console.log.bind(console)
 
-// 工厂模式：无法识别对象的类型
+// 1. 工厂模式：无法识别对象的类型
 {
     function createPerson(name, age, job) {
         var o = new Object()
@@ -24,7 +24,7 @@ var log = console.log.bind(console)
     log("方法内存是否相同：" + (person1.sayName == person2.sayName))
 }
 
-// 构造函数模式：不同实例上的同名函数是不相等的
+// 2. 构造函数模式：不同实例上的同名函数是不相等的
 {
     function Person(name, age, job) {
         this.name = name
@@ -44,7 +44,7 @@ var log = console.log.bind(console)
     log("方法内存是否相同：" + (person1.sayName == person2.sayName))
 }
 
-// 原型模式：在prototype上定义所有实例共享的属性和方法
+// 3. 原型模式：在prototype上定义所有实例共享的属性和方法
 {
     function Person(name, age, job) {
         this.name = name
@@ -76,7 +76,302 @@ var log = console.log.bind(console)
     log(person1.constructor)
 
     //hasOwnProperty
-    console.log("person1.hasOwnProperty(\"name\"):" + (person1.hasOwnProperty("name")))
-    console.log("person1.hasOwnProperty(\"test\"):" + (person1.hasOwnProperty("test")))
-    console.log("person1的prototype.hasOwnProperty(\"test\"):" + (Object.getPrototypeOf(person1).hasOwnProperty("test")))
+    log("person1.hasOwnProperty(\"name\"):" + (person1.hasOwnProperty("name")))
+    log("person1.hasOwnProperty(\"test\"):" + (person1.hasOwnProperty("test")))
+    log("person1的prototype.hasOwnProperty(\"test\"):" + (Object.getPrototypeOf(person1).hasOwnProperty("test")))
+}
+
+// in操作符
+{
+    function Person() {
+    }
+
+    Person.test = "aaa"
+    Person.prototype.name = "Nicholas"
+    Person.prototype.age = 29
+    Person.prototype.job = "Software Engineer"
+    Person.prototype.sayName = function () {
+        log(this.name)
+    }
+    var person1 = new Person()
+    var person2 = new Person()
+
+    log(person1.hasOwnProperty("name")) // false
+    log("name" in person1) // true
+    person1.name = "Greg"
+
+    log(person1.name)
+    log(person1.hasOwnProperty("name"))
+    log("name" in person1)
+
+    function hasPrototypeProperty(object, name) {
+        return !object.hasOwnProperty(name) && (name in object)
+    }
+
+    log(hasPrototypeProperty(person1, "name"))
+
+    // in查询不可枚举属性
+    Object.defineProperty(person1, "height", {
+        value: "180cm",
+        enumerable: false
+    })
+
+    log(("height" in person1)) //返回true，说明查到了这个属性，尽管是不可枚举的
+
+    // for in 枚举实例属性
+    for (var attr in person1) {
+        log(attr, person1[attr])
+    }
+    for (var attr in Person) {
+        log(attr, Person[attr])
+    }
+}
+
+// Object.keys()
+{
+    function Person(name, age, job) {
+        this.name = name
+        this.age = age
+        this.job = job
+    }
+
+    Person.prototype.sayName = function () {
+        log(this.name)
+    }
+    var person1 = new Person("Nicholas", 29, "Software Engineer")
+
+    log(Object.getOwnPropertyDescriptor(Person, "prototype"))
+    var keys = Object.keys(Person.prototype)
+    log(keys)
+}
+
+// Object.getOwnPropertyNames()
+{
+    function Person(name, age, job) {
+
+    }
+
+    Person.prototype.name = "Nick"
+    Person.prototype.age = 25
+    Person.prototype.job = "Software Engineer"
+    Person.prototype.sayName = function () {
+        log(this.name)
+    }
+
+    var keys = Object.getOwnPropertyNames(Person.prototype)
+    log("通过getOwnPropertyNames获取Person.prototype上的属性")
+    log(keys)
+
+    var keys = Object.keys(Person.prototype)
+    log("通过Object.keys获取Person.prototype上的属性")
+    log(keys)
+
+    var keys = Object.getOwnPropertyNames(Person)
+    log("Person的keys：")
+    log(keys)
+
+    var keys = Object.keys(Person)
+    log("通过Object.keys获取Person的属性")
+    log(keys)
+}
+
+// instanceof的理解：
+{
+    function Person() {
+
+    }
+
+    Person.prototype = {
+        name: "Nick",
+        age: 25,
+        job: "Software Engineer",
+        sayName: function () {
+            log(this.name)
+        }
+    }
+
+    var friend = new Person()
+    log(friend instanceof Object)
+    log(friend instanceof Person)
+    log(friend.constructor == Person)
+    log(friend.constructor == Object)
+    log(friend.constructor)// ƒ Object() { [native code] } 丢失了指向
+    log(Person.prototype === Object.getPrototypeOf(friend)) // 这是instanceof仍然成立的原因
+    // Object.defineProperty(Person.prototype, "constructor", {
+    //     enumerable: false,
+    //     value: Person
+    // })
+}
+
+// 重写构造函数
+{
+    function Person() {
+
+    }
+
+    Person.prototype = {
+        name: "Nick",
+        age: 25,
+        job: "Software Engineer",
+        sayName: function () {
+            log(this.name)
+        }
+    }
+
+    Object.defineProperty(Person.prototype, "constructor", {
+        enumerable: false,
+        value: Person
+    })
+}
+
+// 原型的动态性
+{
+    function Friend() {
+
+    }
+
+    var friend = new Friend()
+    log("之前的prototype：")
+
+    log(Object.getPrototypeOf(friend))
+
+
+    Friend.prototype = {
+        name: "Nick",
+        age: 25,
+        job: "Software Engineer",
+        sayName: function () {
+            log(this.name)
+        }
+    }
+
+    Object.defineProperty(Friend.prototype, "constructor", {
+        enumerable: false,
+        value: Friend
+    })
+
+    // friend.sayName() // error
+    log("之后的prototype：")
+    log(Object.getPrototypeOf(friend))
+
+    log("Friend.prototype:")
+    log(Friend.prototype)
+
+    log("friend instanceof Friend:因为二者的prototype已经不一样了")
+    log(friend instanceof Friend)
+
+
+}
+
+// 4. 混合模式的常见写法
+{
+    function Person(name, age, job) {
+        this.name = name
+        this.age = age
+        this.job = job
+        this.friends = ["Shelby", "Court"]
+    }
+
+    Person.prototype = {
+        constructor: Person, // 通常的写法，并不介意constructor是否可枚举
+        sayName: function () {
+            log(this.name)
+        }
+    }
+}
+
+// 5. 动态原型模式
+{
+    function Person(name, age, job) {
+        this.name = name
+        this.age = age
+        this.job = job
+
+        // 方法
+        if (typeof this.sayName !== "function") {
+            Person.prototype.sayName = function () {
+                log(this.name)
+            }
+        }
+    }
+
+    var friend = new Person("Nick", 25, "Software Engineer")
+    friend.sayName()
+}
+
+// 6. 寄生构造函数模式
+{
+    function Person(name, age, job) {
+        var o = new Object()
+        o.name = name
+        o.age = age
+        o.job = job
+        o.sayName = function () {
+            log(this.name)
+        }
+        return o
+    }
+
+    var friend = new Person("Nick", 25, "Software Engineer")
+    friend.sayName()
+
+    // 适用场景：扩展原有的对象
+    function SpecialArray() {
+        var values = new Array()
+        values.push.apply(values, arguments) // 添加值
+        values.toPipedString = function () {
+            return this.join("|")
+        }
+        return values
+    }
+
+    var colors = new SpecialArray("red", "blue", "green")
+    log(colors.toPipedString())
+
+    log(Object.getPrototypeOf(colors) === Array.prototype)
+}
+
+// 7. 稳妥构造函数模式
+{
+    function Person(name, age, job) {
+        // 创建要返回的对象
+        var o = new Object()
+
+        // 定义私有变量和函数
+        // o.name = name
+        // o.age = age
+        // o.job = job
+
+        // 添加方法
+        o.sayName = function () {
+            log(name)
+        }
+
+        return o
+    }
+
+    var friend = Person("Nick", 25, "Software Engineer")
+    friend.sayName()
+}
+
+// 8. ES6的class
+{
+    class Person {
+        constructor(name, age, job) {
+            this.name = name
+            this.age = age
+            this.job = job
+        }
+
+        sayName() {
+            log(this.name)
+        }
+    }
+
+    var person = new Person("Nick", 25, "Software Engineer")
+    person.sayName()
+
+    // constructor的属性检测
+    log("class构建的constructor是否可枚举：")
+    log(Object.getOwnPropertyDescriptor(Person.prototype, "constructor").enumerable) // 不可枚举，完美！
 }
--- server/public/js/index.js
+++ server/public/js/index.js
@@ -80,11 +80,15 @@ __webpack_require__(2);
 /* 2 */
 /***/ (function(module, exports) {
 
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
 //create object
 /* eslint-disable no-redeclare, no-console ,no-unused-vars*/
 var log = console.log.bind(console);
 
-// 工厂模式：无法识别对象的类型
+// 1. 工厂模式：无法识别对象的类型
 {
     var createPerson = function createPerson(name, age, job) {
         var o = new Object();
@@ -106,7 +110,7 @@ var log = console.log.bind(console);
     log("方法内存是否相同：" + (person1.sayName == person2.sayName));
 }
 
-// 构造函数模式：不同实例上的同名函数是不相等的
+// 2. 构造函数模式：不同实例上的同名函数是不相等的
 {
     var Person = function Person(name, age, job) {
         this.name = name;
@@ -126,7 +130,7 @@ var log = console.log.bind(console);
     log("方法内存是否相同：" + (person1.sayName == person2.sayName));
 }
 
-// 原型模式：在prototype上定义所有实例共享的属性和方法
+// 3. 原型模式：在prototype上定义所有实例共享的属性和方法
 {
     var _Person = function _Person(name, age, job) {
         this.name = name;
@@ -158,9 +162,299 @@ var log = console.log.bind(console);
     log(person1.constructor);
 
     //hasOwnProperty
-    console.log("person1.hasOwnProperty(\"name\"):" + person1.hasOwnProperty("name"));
-    console.log("person1.hasOwnProperty(\"test\"):" + person1.hasOwnProperty("test"));
-    console.log("person1的prototype.hasOwnProperty(\"test\"):" + Object.getPrototypeOf(person1).hasOwnProperty("test"));
+    log("person1.hasOwnProperty(\"name\"):" + person1.hasOwnProperty("name"));
+    log("person1.hasOwnProperty(\"test\"):" + person1.hasOwnProperty("test"));
+    log("person1的prototype.hasOwnProperty(\"test\"):" + Object.getPrototypeOf(person1).hasOwnProperty("test"));
+}
+
+// in操作符
+{
+    var _Person2 = function _Person2() {};
+
+    var hasPrototypeProperty = function hasPrototypeProperty(object, name) {
+        return !object.hasOwnProperty(name) && name in object;
+    };
+
+    _Person2.test = "aaa";
+    _Person2.prototype.name = "Nicholas";
+    _Person2.prototype.age = 29;
+    _Person2.prototype.job = "Software Engineer";
+    _Person2.prototype.sayName = function () {
+        log(this.name);
+    };
+    var person1 = new _Person2();
+    var person2 = new _Person2();
+
+    log(person1.hasOwnProperty("name")); // false
+    log("name" in person1); // true
+    person1.name = "Greg";
+
+    log(person1.name);
+    log(person1.hasOwnProperty("name"));
+    log("name" in person1);
+
+    log(hasPrototypeProperty(person1, "name"));
+
+    // in查询不可枚举属性
+    Object.defineProperty(person1, "height", {
+        value: "180cm",
+        enumerable: false
+    });
+
+    log("height" in person1); //返回true，说明查到了这个属性，尽管是不可枚举的
+
+    // for in 枚举实例属性
+    for (var attr in person1) {
+        log(attr, person1[attr]);
+    }
+    for (var attr in _Person2) {
+        log(attr, _Person2[attr]);
+    }
+}
+
+// Object.keys()
+{
+    var _Person3 = function _Person3(name, age, job) {
+        this.name = name;
+        this.age = age;
+        this.job = job;
+    };
+
+    _Person3.prototype.sayName = function () {
+        log(this.name);
+    };
+    var person1 = new _Person3("Nicholas", 29, "Software Engineer");
+
+    log(Object.getOwnPropertyDescriptor(_Person3, "prototype"));
+    var keys = Object.keys(_Person3.prototype);
+    log(keys);
+}
+
+// Object.getOwnPropertyNames()
+{
+    var _Person4 = function _Person4(name, age, job) {};
+
+    _Person4.prototype.name = "Nick";
+    _Person4.prototype.age = 25;
+    _Person4.prototype.job = "Software Engineer";
+    _Person4.prototype.sayName = function () {
+        log(this.name);
+    };
+
+    var keys = Object.getOwnPropertyNames(_Person4.prototype);
+    log("通过getOwnPropertyNames获取Person.prototype上的属性");
+    log(keys);
+
+    var keys = Object.keys(_Person4.prototype);
+    log("通过Object.keys获取Person.prototype上的属性");
+    log(keys);
+
+    var keys = Object.getOwnPropertyNames(_Person4);
+    log("Person的keys：");
+    log(keys);
+
+    var keys = Object.keys(_Person4);
+    log("通过Object.keys获取Person的属性");
+    log(keys);
+}
+
+// instanceof的理解：
+{
+    var _Person5 = function _Person5() {};
+
+    _Person5.prototype = {
+        name: "Nick",
+        age: 25,
+        job: "Software Engineer",
+        sayName: function sayName() {
+            log(this.name);
+        }
+    };
+
+    var friend = new _Person5();
+    log(friend instanceof Object);
+    log(friend instanceof _Person5);
+    log(friend.constructor == _Person5);
+    log(friend.constructor == Object);
+    log(friend.constructor); // ƒ Object() { [native code] } 丢失了指向
+    log(_Person5.prototype === Object.getPrototypeOf(friend)); // 这是instanceof仍然成立的原因
+    // Object.defineProperty(Person.prototype, "constructor", {
+    //     enumerable: false,
+    //     value: Person
+    // })
+}
+
+// 重写构造函数
+{
+    var _Person6 = function _Person6() {};
+
+    _Person6.prototype = {
+        name: "Nick",
+        age: 25,
+        job: "Software Engineer",
+        sayName: function sayName() {
+            log(this.name);
+        }
+    };
+
+    Object.defineProperty(_Person6.prototype, "constructor", {
+        enumerable: false,
+        value: _Person6
+    });
+}
+
+// 原型的动态性
+{
+    var Friend = function Friend() {};
+
+    var friend = new Friend();
+    log("之前的prototype：");
+
+    log(Object.getPrototypeOf(friend));
+
+    Friend.prototype = {
+        name: "Nick",
+        age: 25,
+        job: "Software Engineer",
+        sayName: function sayName() {
+            log(this.name);
+        }
+    };
+
+    Object.defineProperty(Friend.prototype, "constructor", {
+        enumerable: false,
+        value: Friend
+    });
+
+    // friend.sayName() // error
+    log("之后的prototype：");
+    log(Object.getPrototypeOf(friend));
+
+    log("Friend.prototype:");
+    log(Friend.prototype);
+
+    log("friend instanceof Friend:因为二者的prototype已经不一样了");
+    log(friend instanceof Friend);
+}
+
+// 4. 混合模式的常见写法
+{
+    var _Person7 = function _Person7(name, age, job) {
+        this.name = name;
+        this.age = age;
+        this.job = job;
+        this.friends = ["Shelby", "Court"];
+    };
+
+    _Person7.prototype = {
+        constructor: _Person7, // 通常的写法，并不介意constructor是否可枚举
+        sayName: function sayName() {
+            log(this.name);
+        }
+    };
+}
+
+// 5. 动态原型模式
+{
+    var _Person8 = function _Person8(name, age, job) {
+        this.name = name;
+        this.age = age;
+        this.job = job;
+
+        // 方法
+        if (typeof this.sayName !== "function") {
+            _Person8.prototype.sayName = function () {
+                log(this.name);
+            };
+        }
+    };
+
+    var friend = new _Person8("Nick", 25, "Software Engineer");
+    friend.sayName();
+}
+
+// 6. 寄生构造函数模式
+{
+    var _Person9 = function _Person9(name, age, job) {
+        var o = new Object();
+        o.name = name;
+        o.age = age;
+        o.job = job;
+        o.sayName = function () {
+            log(this.name);
+        };
+        return o;
+    };
+
+    // 适用场景：扩展原有的对象
+    var SpecialArray = function SpecialArray() {
+        var values = new Array();
+        values.push.apply(values, arguments); // 添加值
+        values.toPipedString = function () {
+            return this.join("|");
+        };
+        return values;
+    };
+
+    var friend = new _Person9("Nick", 25, "Software Engineer");
+    friend.sayName();
+
+    var colors = new SpecialArray("red", "blue", "green");
+    log(colors.toPipedString());
+
+    log(Object.getPrototypeOf(colors) === Array.prototype);
+}
+
+// 7. 稳妥构造函数模式
+{
+    var _Person10 = function _Person10(name, age, job) {
+        // 创建要返回的对象
+        var o = new Object();
+
+        // 定义私有变量和函数
+        // o.name = name
+        // o.age = age
+        // o.job = job
+
+        // 添加方法
+        o.sayName = function () {
+            log(name);
+        };
+
+        return o;
+    };
+
+    var friend = _Person10("Nick", 25, "Software Engineer");
+    friend.sayName();
+}
+
+// 8. ES6的class
+{
+    var _Person11 = function () {
+        function _Person11(name, age, job) {
+            _classCallCheck(this, _Person11);
+
+            this.name = name;
+            this.age = age;
+            this.job = job;
+        }
+
+        _createClass(_Person11, [{
+            key: "sayName",
+            value: function sayName() {
+                log(this.name);
+            }
+        }]);
+
+        return _Person11;
+    }();
+
+    var person = new _Person11("Nick", 25, "Software Engineer");
+    person.sayName();
+
+    // constructor的属性检测
+    log("class构建的constructor是否可枚举：");
+    log(Object.getOwnPropertyDescriptor(_Person11.prototype, "constructor").enumerable); // 不可枚举，完美！
 }
 
 /***/ })
